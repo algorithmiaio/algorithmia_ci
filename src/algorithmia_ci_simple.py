0838ace10f6b557434d908b1ab8f0d19b8161d08
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-03-23 15:49:46 -0600

    this should cause the build to fail

--- src/algorithmia_ci_simple.py
+++ src/algorithmia_ci_simple.py
@@ -3,4 +3,4 @@ import Algorithmia
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    return "hello {}".format(input)
+    return "hola {}".format(input)
