36b3735556531030335c59ee94ad32a60dc685a4
James Sutton <1892175+zeryx@users.noreply.github.com>
2021-02-18 10:30:37 -0400

    added a comment

--- src/algorithmia_ci_simple.py
+++ src/algorithmia_ci_simple.py
@@ -1,5 +1,6 @@
 import Algorithmia
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
+# adding a comment here
 def apply(input):
     return "hello {}".format(input)
